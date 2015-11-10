6933903dfe7504048b0460c9d363f66933ecc32e
Damián Ariel Betancur <da_ariel@hotmail.com>
2015-11-10 19:52:41 -0300

    carga de archivos iniciales

---
+++ src/mercaditovista/MercaditoVista.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mercaditovista;
+
+import com.l2fprod.gui.plaf.skin.SkinLookAndFeel;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import vista.MarcoContenedor;
+import vista.MarcoProveedor;
+
+/**
+ *
+ * @author Ariel
+ */
+public class MercaditoVista {
+
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MarcoContenedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MarcoContenedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MarcoContenedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MarcoContenedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                /**
+                 * Carga de Skins
+                 */
+                try {
+                    JFrame.setDefaultLookAndFeelDecorated(true);
+                    SkinLookAndFeel.setSkin(SkinLookAndFeel.loadThemePack("temas/mmMagra-Xthemepack.zip"));
+                    UIManager.setLookAndFeel("com.l2fprod.gui.plaf.skin.SkinLookAndFeel");
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, e);
+                }
+                new MarcoContenedor().setVisible(true);
+            }
+        });
+    }
+    
+}
